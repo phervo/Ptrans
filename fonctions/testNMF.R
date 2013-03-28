c9c4e47f0194e016e62129765c29bc65c62970e6
Pierre-Yves Hervo <pierre.yves.hervo@gmail.com>
2013-03-28 11:41:59 +0100

    fonctions de test de NMF

---
+++ fonctions/testNMF.R
@@ -0,0 +1,11 @@
+library(NMF)
+library(pkgmaker)
+library(rngtools)
+library(registry)
+library(digest)
+library(grid)
+n = 20
+counts= c(5, 3, 2)
+p = sum(counts)
+x = syntheticNMF(n, counts)
+dim(x)
